a31f2f8975952efc37110cca25d45fc1c529d3ed
Daniel Berthran <berthlearn@gmail.com>
2023-03-09 13:20:39 -0800

    pycodestyle error 0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import add_0
+
+
 def main():
     add = add_0.add
-    print("1 + 2 = {}".format(add(1,2)))
+    print("1 + 2 = {}".format(add(1, 2)))
+
+
 if __name__ == "__main__":
     main()
