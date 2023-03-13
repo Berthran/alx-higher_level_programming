81318208e3341b748c461bcc45582f04f6a814c7
Daniel Berthran <berthlearn@gmail.com>
2023-03-13 10:31:51 +0100

    returns 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
     no_elements = len(my_list)
     for i in range(no_elements):
         print("{}".format(my_list[i]))
+    return (0)
 
 
 if __name__ == "__main__":
