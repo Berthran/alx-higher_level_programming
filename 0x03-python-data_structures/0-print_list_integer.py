10175cd8af1e18d9c4e78026d3fa9f0087b4e172
Daniel Berthran <berthlearn@gmail.com>
2023-03-13 10:16:58 +0100

    run with import 0-print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,7 +4,3 @@ def print_list_integer(my_list=[]):
     no_elements = len(my_list)
     for i in range(no_elements):
         print("{}".format(my_list[i]))
-
-
-if __name__ == "__main__":
-    print_list_integer()
