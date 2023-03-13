9be72c555cc764343f82bf44891dc9fdab942da6
Daniel Berthran <berthlearn@gmail.com>
2023-03-13 13:48:01 +0100

    fixing error 4 task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,9 +7,9 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(no_rows):
         for j in range(no_cols):
             if j < no_cols - 1:
-                print("{}".format(matrix[i][j]), end=" ")
+                print("{:d}".format(matrix[i][j]), end=" ")
             else:
-                print("{}".format(matrix[i][j]), end="")
+                print("{:d}".format(matrix[i][j]), end="")
         print()
 
 
