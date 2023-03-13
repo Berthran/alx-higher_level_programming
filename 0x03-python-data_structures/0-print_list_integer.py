f4f27d3be552d000a0ebcdf0a90ce6e8a6cd9f43
Daniel Berthran <berthlearn@gmail.com>
2023-03-13 10:28:00 +0100

    increase permissions

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,8 @@
 def print_list_integer(my_list=[]):
     no_elements = len(my_list)
     for i in range(no_elements):
-        print(f"{my_list[i]}")
+        print("{}".format(my_list[i]))
 
+
+if __name__ == "__main__":
+    print_list_integer(my_list=[])
