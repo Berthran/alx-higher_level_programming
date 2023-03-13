fa682802d22ae9361c255538e4eaa90a0180bc71
Daniel Berthran <berthlearn@gmail.com>
2023-03-13 14:00:16 +0100

    list deletion by indexing

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,14 +5,10 @@ def delete_at(my_list=[], idx=0):
     new_list = []
 
     if idx < 0 or idx > list_idx_range:
-        return(my_list)
+        return (my_list)
     else:
-        for i in range(list_idx_range + 1):
-            if i != idx:
-                my_list[i] = my_list[i]
-            else:
-                my_list[i] = my_list[i + 1]
-    return(my_list)
+        my_list[:] = my_list[:idx] + my_list[idx + 1:]
+    return (my_list)
 
 
 if __name__ == "__main__":
