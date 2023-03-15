cfe4ffc29c4147486e8128079eb03c64981114b8
Daniel Berthran <berthlearn@gmail.com>
2023-03-15 14:55:41 +0100

    search and replace -pycodestyle fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    ''' Replaces all occurences of an element by another element in a new list '''
+    ''' Replaces all occurences of an element
+    by another element in a new list '''
     new_list = []
     for ele in my_list:
         if ele == search:
             new_list.append(replace)
         else:
             new_list.append(ele)
-    return(new_list)
+    return (new_list)
