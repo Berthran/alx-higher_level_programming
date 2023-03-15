e5accf49b3bfb291c2034b0df29e8c6c84fccd4d
Daniel Berthran <berthlearn@gmail.com>
2023-03-15 14:54:06 +0100

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    ''' Replaces all occurences of an element by another element in a new list '''
+    new_list = []
+    for ele in my_list:
+        if ele == search:
+            new_list.append(replace)
+        else:
+            new_list.append(ele)
+    return(new_list)
