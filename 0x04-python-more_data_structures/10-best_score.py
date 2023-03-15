2258e43e8f97b7a8f3192bd7ae9b2d124c366a95
Daniel Berthran <berthlearn@gmail.com>
2023-03-15 16:41:08 +0100

    dict keys biggest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    ''' returns the key of the dictionary with the highest value '''
+    if a_dictionary is None:
+        return (None)
+    first_key = list(a_dictionary)[0]
+    prev_score = a_dictionary[first_key]
+    best_key = ""
+    for key, value in a_dictionary.items():
+        if value > prev_score:
+            best_key = key
+            prev_score = value
+    return (best_key)
