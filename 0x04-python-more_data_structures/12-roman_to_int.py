b3e1f06d98a1640ec3a0ec2b288a7d4788b0ffb2
Daniel Berthran <berthlearn@gmail.com>
2023-03-15 15:52:29 +0100

    from roman numeral to integer - None and str fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,10 @@
 def roman_to_int(roman_string):
     r_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     r_int = []
+    if roman_string is None or type(roman_string) != str:
+        return(0)
     for i in roman_string:
-        if i not in r_num or i is None or type(i) != str:
-            return (0)
-        else:
-            r_int.append(r_num[i])
+        r_int.append(r_num[i])
     int_sum = r_int[0]
     for i in range(len(r_int) - 1):
         a = r_int[i]
