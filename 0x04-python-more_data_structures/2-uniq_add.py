8e4e4a8fac313aa1786c46287edcddca2fd4e270
Daniel Berthran <berthlearn@gmail.com>
2023-03-15 15:03:05 +0100

    sum of 'set' elements

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    ''' adds all unique integers in a list'''
+    uniq_list = set(my_list)
+    uniq_sum = 0
+    for i in uniq_list:
+        uniq_sum += i
+    return (uniq_sum)
