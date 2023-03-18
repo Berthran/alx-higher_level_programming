16583e3a2a419c2d5f75aadc825ce2a5623dbe67
Daniel Berthran <berthlearn@gmail.com>
2023-03-18 14:11:11 +0100

    check cycle in singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if singly linked list has a cycle in it
+ * @list: pointer to beginnig of the list
+ *
+ * Return: 1, if there's a circle, 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	int i, address_counter = 1;
+	listint_t *list_crawler;
+	char **address_store = (char **)malloc(sizeof(char *));
+
+	if (list == NULL || address_store == NULL)
+		return (0);
+	list_crawler = list;
+	address_store[0] = (char *)list_crawler;
+	list_crawler = list_crawler->next;
+
+	while (list_crawler)
+	{
+		for (i = 0; i < address_counter; i++)
+		{
+			if (address_store[i] == (char *)list_crawler)
+			{
+				free(address_store);
+				return (1); /* Cycle found */
+			}
+		}
+		address_store[address_counter] = (char *)list_crawler;
+		address_counter += 1;
+		list_crawler = list_crawler->next;
+	}
+	free(address_store);
+	return (0); /* Cycle not found */
+}
