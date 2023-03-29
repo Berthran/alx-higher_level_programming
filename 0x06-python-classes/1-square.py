2aeb979a37b6fc162ce8de87f49f566fcfa7501d
Daniel Berthran <berthlearn@gmail.com>
2023-03-29 05:30:27 +0100

    set private attribute for a class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,40 @@
 #!/usr/bin/python3
 
+'''A simple demonstration of Python Class Attributes.
+Classes:
+--------
+
+    Square
+
+Typical Usage Example:
+---------------------
+
+    small_square = Square(1)
+    large_square = Square(100)
+'''
+
+
+class Square:
+    '''
+    A class to create a square
+
+    ...
+
+    Attributes:
+    -----------
+        size : None
+            size of the square
+
+    '''
+
+    def __init__(self, size):
+        """
+        Constructs all the necessary attributes for a square object.
+
+        Parameters:
+        -----------
+            size : None
+                size of the square
+        """
+
+        self.__size = size
