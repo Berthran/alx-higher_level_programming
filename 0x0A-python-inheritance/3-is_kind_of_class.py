10f353304e808d45c2a756a367db09c417d31b70
Daniel Berthran <berthlearn@gmail.com>
2023-04-11 21:14:00 +0100

    Proper documentation 3-is_kind_of_class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+'''An instance object boolean checker
+
+Methods:
+    is_kind_of_class : Returns True if condition is met else False
+
+'''
 
 
 def is_kind_of_class(obj, a_class):
+    '''Returns True if "obj" is an instance of "a_class"'''
     if (isinstance(obj, a_class)):
         return (True)
     return (False)
