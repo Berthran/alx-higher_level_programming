c8505e7f6691898d4fb28bc792d13b0ae8182066
Daniel Berthran <berthlearn@gmail.com>
2023-04-11 23:23:07 +0100

    Solve Geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''An illustration of class inheritance
+
+Classes:
+    BaseGeometry : representation of basic Geometry
+
+'''
+
+
+class BaseGeometry:
+    '''An empty class'''
+    pass
