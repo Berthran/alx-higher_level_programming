11f658f1f71e0722d739c7b6eef72c3b2e24271c
Daniel Berthran <berthlearn@gmail.com>
2023-04-11 21:18:01 +0100

    Proper documentation 4-inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+'''An instance object boolean checker III.
+
+Methods:
+    inherits_from  : Returns True is condition is met else False
+'''
 
 
 def inherits_from(obj, a_class):
+    '''Returns True if "obj" is instance of "a_class"'''
     if (isinstance(obj, a_class) and issubclass(a_class, a_class)):
         return (True)
     return (False)
