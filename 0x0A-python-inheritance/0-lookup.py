87ffcb2ee247c8df0e870ce4626e856e28a8c247
Daniel Berthran <berthlearn@gmail.com>
2023-04-11 18:00:18 +0100

    documenting 0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+'''Displays the attributes and methods of an object'''
 
 def lookup(obj):
+    '''Function that shows an object's methods and attributes'''
     return (dir(obj))
