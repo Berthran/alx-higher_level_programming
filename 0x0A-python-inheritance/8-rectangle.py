90b6054c08733d69c33d408ffd9cf99b94326ceb
Daniel Berthran <berthlearn@gmail.com>
2023-04-12 05:31:39 +0100

    Solve 8-rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Class Inheritance.
+
+'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Creates a rectangle using attributes from BaseGeometry'''
+
+    def __init__(self, width, height):
+        '''Instantiates class with required attributes
+
+        Parameters:
+            width: width of rectangle (private property)
+            height ; height of rectangel (private property)
+        '''
+        if (not self.integer_validator("width", width)):
+            self.__width = width
+        if (not self.integer_validator("height", height)):
+            self.__height = height
