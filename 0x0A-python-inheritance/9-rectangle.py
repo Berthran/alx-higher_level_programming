e73fc1c56bdcee2b0d94a12b295ce57ca266b2b8
Daniel Berthran <berthlearn@gmail.com>
2023-04-12 06:03:28 +0100

    requirement fix I

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -16,7 +16,7 @@ Typical Example Usage:
 
 '''
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+#BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
