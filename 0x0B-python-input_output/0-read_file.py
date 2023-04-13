588bcd2aaf3867973acb6bc8f99e693b7cfbb633
Daniel Berthran <berthlearn@gmail.com>
2023-04-13 04:48:55 +0100

    Solved 0-read_file - read and prints file content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Read file.
+'''
+
+def read_file(filename=""):
+    '''Reads file "filename and prints the content"'''
+    with open(filename, "r", encoding="utf-8") as f:
+        file_contents = f.read()
+        print(file_contents)
