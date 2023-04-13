ec016529510687a568b24cb3d6888913fe3f1c2b
Daniel Berthran <berthlearn@gmail.com>
2023-04-13 05:43:07 +0100

    pycodestyle fix 0-read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,9 @@
 '''Read file.
 '''
 
+
 def read_file(filename=""):
     '''Reads file "filename and prints the content"'''
     with open(filename, "r") as f:
         file_contents = f.read()
-        print(file_contents)
+    print(file_contents)
