5d98aa90633555bb225326f8e17c293a4afc6c5a
Daniel Berthran <berthlearn@gmail.com>
2023-05-19 17:11:59 +0100

    How to use alias using (AS)

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,7 +2,7 @@
 -- Displays the score
 -- Displays the number of records for that score with a label number
 -- Displays a sorted list in descending order
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
 
 -- This works too
 -- SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
