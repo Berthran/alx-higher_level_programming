fb4e8533b4a0515dbefe09622ffcf5af6f63815a
Daniel Berthran <berthlearn@gmail.com>
2023-05-19 17:08:24 +0100

    How to use GROUP BY for aggregate functions

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- Lists the number of records with the same score from the second_table
+-- Displays the score
+-- Displays the number of records for that score with a label number
+-- Displays a sorted list in descending order
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+
+-- This works too
+-- SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
