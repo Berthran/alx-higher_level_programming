eca6b67550ef1eaa70df871b3a594ffd35d6da98
Daniel Berthran <berthlearn@gmail.com>
2023-05-19 16:36:44 +0100

    How to filter and sort a table by a columns values

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the second_table of the hbtn_0c_0 database
+--  Only the score and name columns are displayed
+-- The scores are displayed in an descending order
+SELECT score, name FROM second_table ORDER BY score DESC;
