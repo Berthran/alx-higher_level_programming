79631b575774f48803a857d6635cb383ded5da84
Chika B. Daniel <chikabdaniel@gmail.com>
2023-10-31 03:30:50 +0100

    code fix I 0-rectangle

---
+++ 0x08-python-more_classes/test_files/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append("/home/danlinux/new-ALX_SE/alx-higher_level_programming/0x08-python-more_classes")
+Rectangle = __import__("0-rectangle").Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
