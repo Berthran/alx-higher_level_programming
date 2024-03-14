06de598486f93bc209928267782e6985c877fa1d
Daniel Berthran <berthlearn@gmail.com>
2024-03-14 11:41:10 +0100

    spaced out code for easy readability

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,11 +5,14 @@ import sys
 def main():
     argv = sys.argv
     no_argv = len(sys.argv) - 1
+
     i = 1
     sum = 0
+
     if no_argv > 0:
         for i in range(1, no_argv + 1):
             sum += int(argv[i])
+    
     print("{}".format(sum))
 
 
