89bb8a14e88be7711da612742802cef919dc5eb8
Daniel Berthran <berthlearn@gmail.com>
2024-03-17 23:37:46 +0100

    correct file name + remove declaration from header

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,8 +21,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 
 void free_listint(listint_t *head);
 
-int count_nodes(const listint_t *h);
-
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
