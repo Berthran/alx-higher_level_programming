ad2250a2bb0ee129829ac076ec0eb8503bc3abcf
Daniel Berthran <berthlearn@gmail.com>
2024-03-17 21:33:55 +0100

    pycodestyle fix task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,10 +4,10 @@
 def element_at(my_list, idx):
     list_idx_range = len(my_list) - 1
     if idx < 0:
-        return(None)
+        return (None)
     if idx > list_idx_range:
-        return(None)
-    return(my_list[idx])
+        return (None)
+    return (my_list[idx])
 
 
 if __name__ == "__main__":
