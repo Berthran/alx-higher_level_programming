756ec4ebe6421d464a17a00d352ddf861ba90b1f
Daniel Berthran <berthlearn@gmail.com>
2024-03-18 00:10:18 +0100

    force error detection

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,14 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: beginning if the singly linked list
+ * insert_node - adds a node to a sorted linked list
+ * @head: the head of the linked list
+ * @number: integer value of new node
  *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Return: address of new node
  */
 
-int is_palindrome(listint_t **head)
+listint_t *insert_node(listint_t **head, int number)
 {
-	printf("Successful\n");
-	return (0);
+	listint_t *node_crawler, *prev_node;
+	int n, node_counter = 1;
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+
+	if (head == NULL || new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+	if (*head == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+	node_crawler = *head;
+
+	while (node_crawler)
+	{
+		n = node_crawler->n;
+		if (n > number)
+		{
+			new_node->next = node_crawler;
+			if (node_counter == 1)
+			{
+				*head = new_node;
+				return (*head);
+			}
+			prev_node->next = new_node;
+			return (*head);
+		}
+		prev_node = node_crawler;
+		node_crawler = node_crawler->next;
+		node_counter += 1;
+	}
+	prev_node->next = new_node;
+	return (*head);
 }
