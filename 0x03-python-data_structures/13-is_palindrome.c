35ee24c9dc48b5ae0cc30c14d0c4bf2667844db1
Daniel Berthran <berthlearn@gmail.com>
2024-03-18 00:14:42 +0100

    Error detection continues

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,51 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
+
+int count_nodes(const listint_t *h);
+
+int compare_list(listint_t *l1, listint_t *l2, int n);
 
 /**
- * insert_node - adds a node to a sorted linked list
- * @head: the head of the linked list
- * @number: integer value of new node
- *
- * Return: address of new node
+ * count_nodes - counts nodes
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
 
-listint_t *insert_node(listint_t **head, int number)
+int count_nodes(const listint_t *h)
 {
-	listint_t *node_crawler, *prev_node;
-	int n, node_counter = 1;
-	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+	const listint_t *current;
+	int n;
 
-	if (head == NULL || new_node == NULL)
-		return (NULL);
+	current = h;
+	n = 0;
 
-	new_node->n = number;
-	if (*head == NULL)
+	while (current != NULL)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (*head);
+		current = current->next;
+		n++;
 	}
-	node_crawler = *head;
-
-	while (node_crawler)
-	{
-		n = node_crawler->n;
-		if (n > number)
-		{
-			new_node->next = node_crawler;
-			if (node_counter == 1)
-			{
-				*head = new_node;
-				return (*head);
-			}
-			prev_node->next = new_node;
-			return (*head);
-		}
-		prev_node = node_crawler;
-		node_crawler = node_crawler->next;
-		node_counter += 1;
-	}
-	prev_node->next = new_node;
-	return (*head);
+	return (n);
 }
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: beginning if the singly linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int i,  nodes, nodes_split;
+	listint_t *crawler, *first_half, *second_half;
+
+	if (*head == NULL)
+		return (1);
+
+	/* Palindrome has to have even nodes */
+	nodes = count_nodes(*head);
+	nodes_split = nodes / 2;
+	if (nodes % 2 != 0)
+		return (0);
+
+	first_half = crawler = *head;
+
+	for (i = 1; i < nodes_split; ++i)
+		crawler = crawler->next;
+
+	second_half = crawler->next;
+	crawler->next = NULL;
+
+	if (compare_list(first_half, second_half, nodes_split) == 0)
+		return (0);
+	return (1);
+}
+
+
+/**
+ * compare_list - compares the value of two linked lists
+ * @l1: the first linked list
+ * @l2: the second linked list
+ * @n: the number of nodes in each list
+ *
+ * Return: 1 if all the nodes have thesame value for both linked lists
+ */
+
+int compare_list(listint_t *l1, listint_t *l2, int n)
+{
+	int i, j, n1, n2;
+	listint_t *c1, *c2;
+
+	c1 = l1;
+	c2 = l2;
+
+	for (i = 0; i < n; i++)
+	{
+		n1 = c1->n;
+		for (j = 0; j < (n - i - 1); j++)
+			c2 = c2->next;
+
+		n2 = c2->n;
+
+		if (n1 != n2)
+			return (0);
+
+		c1 = c1->next;
+		c2 = l2;
+	}
+	return (1);
+}
+
