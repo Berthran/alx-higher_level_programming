cb5967d10f70daa604f39d20c47098f0ce986714
Daniel Berthran <berthlearn@gmail.com>
2024-03-19 11:19:30 +0100

    Use sum rule to determine palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,6 +5,10 @@ int count_nodes(const listint_t *h);
 
 int compare_list(listint_t *l1, listint_t *l2, int n);
 
+int compare_odd(listint_t *head, int nodes);
+
+int compare_even(listint_t *head, int nodes);
+
 /**
  * count_nodes - counts nodes
  * @h: pointer to head of list
@@ -36,19 +40,25 @@ int count_nodes(const listint_t *h)
 
 int is_palindrome(listint_t **head)
 {
-	int i,  nodes, nodes_split;
-	listint_t *crawler, *first_half, *second_half;
+	/*listint_t *crawler; * *first_half, *second_half;*/
+	int nodes, is_p;
 
+	/* Empty list is a palindrome */
 	if (*head == NULL)
 		return (1);
 
-	/* Palindrome has to have even nodes */
 	nodes = count_nodes(*head);
+	/* List with one item is a palindrome */
 	if (nodes == 1)
 		return (1);
-	nodes_split = nodes / 2;
-	if (nodes % 2 != 0)
-		return (0);
+
+	/* Determine if nodes are even or odd */
+	if (nodes % 2 == 0) /* even */
+		is_p = compare_even(*head, nodes);
+	else
+		is_p = compare_odd(*head, nodes);
+
+	/*nodes_split = (nodes / 2);
 
 	first_half = crawler = *head;
 
@@ -60,7 +70,8 @@ int is_palindrome(listint_t **head)
 
 	if (compare_list(first_half, second_half, nodes_split) == 0)
 		return (0);
-	return (1);
+	return (1);*/
+	return (is_p);
 }
 
 
@@ -97,5 +108,75 @@ int compare_list(listint_t *l1, listint_t *l2, int n)
 	}
 	free_listint(l2);
 	return (1);
+}
+
+
+/**
+ * compare_odd - checks if an odd numbered linked list is a palindrome
+ * @head: the pointer to the linked list
+ * @nodes: the number of nodes in the linked list
+ *
+ * Return: 1 if it is a palindrome or 0 if not
+ */
+
+int compare_odd(listint_t *head, int nodes)
+{
+	listint_t *crawler; /* *first_half, *second_half;*/
+	int i, jump = nodes / 2;
+	int sum = 0;
+	crawler = head;
+
+	for (i = 0; i < nodes; i++)
+	{
+		if (i == jump)
+			crawler = crawler->next;
+		else if (i < jump)
+		{
+			sum += crawler->n;
+			crawler = crawler->next;
+		}
+		else if (i > jump)
+		{
+			sum -= crawler->n;
+			crawler = crawler->next;
+		}
+	}
+	if (sum == 0)
+		return (1);
+	return (0);
+}
+
+
+/**
+ * compare_even - checks if an even numbered linked list is a palindrome
+ * @head: the pointer to the linked list
+ * @nodes: the number of nodes in the linked list
+ *
+ * Return: 1 if it is a palindrome or 0 if not
+ */
+
+int compare_even(listint_t *head, int nodes)
+{
+	listint_t *crawler;
+	int i, half = (nodes / 2) - 1;
+	int sum = 0;
+	crawler = head;
+
+	for (i = 0; i < nodes; i++)
+	{
+		if (i <= half)
+		{
+			sum += crawler->n;
+			crawler = crawler->next;
+		}
+		else if (i > half)
+		{
+			sum -= crawler->n;
+			crawler = crawler->next;
+		}
+	}
+	if (sum == 0)
+		return (1);
+	return (0);
 }
 
