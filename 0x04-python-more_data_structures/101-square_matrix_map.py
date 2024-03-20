3159ac398570d3ca9b3917d05b93ec22a74261b1
Daniel Berthran <berthlearn@gmail.com>
2024-03-20 19:38:32 +0100

    map error

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,5 +2,5 @@
 def square_matrix_map(matrix=[]):
     for i in range(2):
         pass
-    return (list(map(lambda l: list(map(lambda i: i**2, l)), matrix)))
+    # return (list(map(lambda l: list(map(lambda i: i**2, l)), matrix)))
 # Newline
