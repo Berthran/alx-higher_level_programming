f471a61a03ff7e7d62944dac08ff73ce27fd354f
Daniel Berthran <berthlearn@gmail.com>
2024-03-20 19:37:34 +0100

    no pycodestyel error, for loop error, extra line error

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda l: list(map(lambda i: i**2, l)), matrix))) #addding more
+    for i in range(2):
+        pass
+    return (list(map(lambda l: list(map(lambda i: i**2, l)), matrix)))
 # Newline
