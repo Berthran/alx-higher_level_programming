70a33bc71e7a73737db562ca09040b4d8dddb788
Daniel Berthran <berthlearn@gmail.com>
2024-03-20 19:44:39 +0100

    remove all forced errors and make executable

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-import ctypes
 def square_matrix_map(matrix=[]):
-    for i in range(2):
-        pass
-# Newline
+    return (list(map(lambda l: list(map(lambda i: i**2, l)), matrix)))
