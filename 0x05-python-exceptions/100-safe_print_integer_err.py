7881bc67ba27d85dbe1151cfe4c3a05ecb4e0172
Daniel Berthran <berthlearn@gmail.com>
2024-04-02 16:45:34 +0100

    use Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,9 +9,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError as vexc:
-        print("Exceptions: ", vexc, file=sys.stderr)
-        return (False)
-    except TypeError as texc:
-        print("Exceptions: ", texc, file=sys.stderr)
+    except Exception as exc:
+        print("Exception: ", exc, file=sys.stderr)
         return (False)
