d976a7f9732a78db7fc633f12f8daf83c7177c23
Daniel Berthran <berthlearn@gmail.com>
2024-04-02 16:36:34 +0100

    separate TypeError and ValueError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,8 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except (ValueError, TypeError) as vexc:
+    except ValueError as vexc:
         print("Exception: ", vexc, file=sys.stderr)
+    except TypeError as texc:
+        print("Exception: ", texc, file=sys.stderr)
     return (False)
