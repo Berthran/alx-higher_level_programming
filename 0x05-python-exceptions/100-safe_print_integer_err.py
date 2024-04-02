388c3ddc9209684a279d5ad1589b1e3fdc979e6d
Daniel Berthran <berthlearn@gmail.com>
2024-04-02 16:46:33 +0100

    not printing to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,13 +2,11 @@
 
 import sys
 
-# 8 11 14 pass
-# 6 7 9 10 12 13 fail
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
     except Exception as exc:
-        print("Exception: ", exc, file=sys.stderr)
+        print("Exception: ", exc)
         return (False)
