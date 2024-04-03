d3e072a9ab4e67a94cbdbc319fdd0f94adfd4b68
Daniel Berthran <berthlearn@gmail.com>
2024-04-03 03:33:08 +0100

    try with format printing again

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,10 +5,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        val = int(value)
-        print("{:d}".format(val))
+        print("{:d}".format(value))
         return (True)
     except Exception as exc:
-        with redirect_stdout(sys.stderr):
-            print("Exception: {}".format(exc), file=sys.stderr)
+        print("Exception: {}".format(exc), file=sys.stderr)
         return (False)
