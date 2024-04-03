87ea3066854b98519f42d424002c6dcdc72b8e14
Daniel Berthran <berthlearn@gmail.com>
2024-04-03 03:30:27 +0100

    try to convert value to int

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        val = int(value)
+        print("{:d}".format(val))
         return (True)
     except Exception as exc:
         with redirect_stdout(sys.stderr):
