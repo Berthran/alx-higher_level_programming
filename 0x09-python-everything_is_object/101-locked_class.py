2a877e13b8c18017785904634ccf44238441c10e
Daniel Berthran <berthlearn@gmail.com>
2024-04-10 04:44:02 +0100

    lock dynamic instance attribute creation

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Module to show how to limit dynamic instance creation in Python
+'''
+
+
+class LockedClass():
+    '''
+    A simple Class representation of how the __setattr__ function works
+    '''
+
+    def __setattr__(self, name, value):
+        '''
+        A function that handles dynamic setting of instance attributes
+        '''
+        if (name != "first_name"):
+            err_msg = f"'LockedClass' Object has no attribute {name}"
+            raise AttributeError(err_msg)
+        super().__setattr__(name, value)
