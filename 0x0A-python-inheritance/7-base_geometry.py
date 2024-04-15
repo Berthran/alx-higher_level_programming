f5d5bc330ce515634dee7a39ff68321279ac28e3
Daniel Berthran <berthlearn@gmail.com>
2024-04-16 00:23:59 +0100

    incorrect message for <= 0

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -33,6 +33,6 @@ class BaseGeometry:
             value: an integer value
         '''
         if (type(value) is not int):
-            raise TypeError("{} must be an integers".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{} must be greater thans 0".format(name))
