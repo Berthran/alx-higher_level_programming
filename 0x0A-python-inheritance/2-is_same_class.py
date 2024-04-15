56dd8372f38fb8c849d78d54eb124c112b078ffc
Daniel Berthran <berthlearn@gmail.com>
2024-04-15 17:05:39 +0100

    correct documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 '''
-A simple demonstration of the use of `isinstance` function
+A simple demonstration of the use of `type` function
+to find if an object is an exact instance of a class
 
 Functions:
     is_same_class: returns true if an object is an instance
     of a specified object
+         return (self.sort
 '''
 
 
@@ -13,10 +15,11 @@ def is_same_class(obj, a_class):
     returns true if an object is an instance of a specified object
 
     Parameters:
-        obj: the object to check if its an instance
+        obj: the object to check if its an exact instance of a_class
         a_class: the name of the specified object to check obj against
     '''
-
+    # obj is an exact instance of a_class
+    # obj is not an instance of a sub_class or a_class
     if (type(obj).__name__ == a_class.__name__):
         return (True)
     return (False)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-A module to demonstrate an instance of a class or a subclass
+A module to demonstrate the use of `instance` function to
+determine if an object is an instance of a class or its sub-classes
 
 Functions:
     is_kind_of_class: returns true for an instance or subclass of a
@@ -17,4 +18,6 @@ def is_kind_of_class(obj, a_class):
         obj: the object to check if its an instance of the speicified object
         a_class: the name of the specified object to check obj against
     '''
+    # Obj is an instance of a_class or an instance of a class that inherits
+    # directly or indirectly form a_class
     return (isinstance(obj, a_class))
