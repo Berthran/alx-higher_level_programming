ad876936552056e10d7a750bc7af462536989fc5
Daniel Berthran <berthlearn@gmail.com>
2024-04-15 17:18:17 +0100

    create base class for inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr.bin/python3
+'''
+A module containing a base class that will serve as the
+the parent class from which other classes can inherit
+
+Classes:
+    BaseGeometry: a class that defines basic features of geometric shapes
+'''
+
+
+class BaseGeometry:
+    '''
+    a class that defines basic features of geometric shapes
+    '''
