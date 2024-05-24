88c46199519bf8485fb8fb7da5917a2f80df15b4
Daniel Berthran <berthlearn@gmail.com>
2024-05-24 16:16:28 +0100

    create a table in the current database running in the MySQL server

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Create a table called first_table in the current database
+-- in the running MySQL server.
+-- If the table already exists, do nothing.
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+	);
