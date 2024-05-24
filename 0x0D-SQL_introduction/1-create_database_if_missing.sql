414e5c0d1695b6088c567950639a839e1a7ad0ac
Daniel Berthran <berthlearn@gmail.com>
2024-05-24 15:13:46 +0100

    create a database if it doesn't exists and do nothing if it exists.

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- A script that creates a database named "hn=btn_0c_0 in the
+-- running MySQL server.
+-- It creates the database if it does not exist, and does nothing
+-- if it exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
