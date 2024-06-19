4c3cc424fdc8586de89b542181ef92ac250bff3f
Daniel Berthran <berthlearn@gmail.com>
2024-06-19 13:06:03 +0100

    Funtion modification

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function add(a, b) {
-	console.log(a + b);
+function add (a, b) {
+  return (a + b);
 }
 
 const firstArg = parseInt(process.argv.slice(2)[0]);
 const secondArg = parseInt(process.argv.slice(2)[1]);
-add(firstArg, secondArg);
+console.log(add(firstArg, secondArg));
