8d7b34a27bd2f7458e92a6b3a4bb3e7ce1dcb5f2
Daniel Berthran <berthlearn@gmail.com>
2024-07-09 12:30:33 +0100

    Display the body of  HTTP response, handling redirections and status code - no redirections after checking status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL as argument, sends a GET request and displays the body of the response
-curl -s -L -w "%{http_code}" "$1" | grep -q 200 && curl -s -L "$1"
+curl -s -L -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
