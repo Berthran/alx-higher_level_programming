a3d3d3d03a604ac8247c4508161af23e0461c3fe
Daniel Berthran <berthlearn@gmail.com>
2024-07-30 14:21:20 +0100

    accessing attributes of a JSON object

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
+
+// Making a GET request
+request(url, function (err, response, body) {
+  if (err) {
+    console.error('code:', response.statusCode);
+  } else {
+    for (let i = 0; i < JSON.parse(body).count; i++) {
+      if (JSON.parse(body).results[i].characters.includes(charUrl)) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
