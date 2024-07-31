3e1f9e036d24aa97f395f061674bbd15bf79d1ef
Daniel Berthran <berthlearn@gmail.com>
2024-07-31 14:54:38 +0100

    Using requestPromise wrappers

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,35 +1,45 @@
 #!/usr/bin/node
-const request = require('request-promise');
+const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 let characters = {};
 
+// TRIAL 1
 // Making a GET request
-request(url)
-  .then(body => {
-    characters = JSON.parse(body).characters;
-    const requests = characters.map(character => request(character));
-    return Promise.all(requests);
-  })
-  .then(characterResponses => {
-    characterResponses.forEach(character => {
-      console.log(JSON.parse(character).name);
-    });
-  })
-  .catch(err => console.error('code:', err.statusCode));
-// request(url, function (err, response, body) {
-//   if (err) {
-//     console.error('code:', response.statusCode);
-//   } else {
+// request(url)
+//   .then(body => {
 //     characters = JSON.parse(body).characters;
-//     for (let i = 0; i < characters.length; i++) {
-//       request(characters[i], function (err, response, body) {
-//         if (err) {
-//           console.error('code:', response.statusCode);
-//         } else {
-//           console.log(JSON.parse(body).name);
-//         }
-//       });
-//     }
-//   }
-// });
+//     const requests = characters.map(character => request(character));
+//     return Promise.all(requests);
+//   })
+//   .then(characterResponses => {
+//     characterResponses.forEach(character => {
+//       console.log(JSON.parse(character).name);
+//     });
+//   })
+//   .catch(err => console.error('code:', err.statusCode));
+
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, function (err, response, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+(async () => {
+  try {
+    const body = await requestPromise(url);
+    characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      const character = await requestPromise(characters[i]);
+      console.log(JSON.parse(character).name);
+    }
+  } catch (err) {
+    console.error('code:', err.statusCode);
+  }
+})();
