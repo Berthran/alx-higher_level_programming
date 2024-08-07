89406debc4e39eecd3774c4f64efca7d24bf4498
Daniel Berthran <berthlearn@gmail.com>
2024-08-07 18:10:39 +0100

    add a toggleClass action on click event

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+// Add comment to each line of code
+// Add a click event to the element with the id toggle_header
+$(function () {
+  if (!$('header').hasClass('red') && !$('header').hasClass('green')) { $('header').addClass('red'); }
+  $('#toggle_header').click(function () {
+    $('header').toggleClass('green red');
+  });
+});
