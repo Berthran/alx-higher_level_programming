9267245541793f5983a4e02a9d07478b80e8620e
Daniel Berthran <berthlearn@gmail.com>
2024-08-07 18:11:17 +0100

    update comment of 4-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,4 +1,3 @@
-// Add comment to each line of code
 // Add a click event to the element with the id toggle_header
 $(function () {
   if (!$('header').hasClass('red') && !$('header').hasClass('green')) { $('header').addClass('red'); }
