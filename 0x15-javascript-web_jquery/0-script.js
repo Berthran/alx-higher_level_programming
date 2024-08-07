a835e3f8716d7debd84219fd0bbf50c52e9681d0
Daniel Berthran <berthlearn@gmail.com>
2024-08-07 16:56:13 +0100

    No JQuery DOM manipulation

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// This line of code selects the first header element in the document
+const header = document.querySelector("header");
+// This line of code changes the color of the header element to red
+header.style = "color: #FF0000";
